c2416d273b13440b75ed535a4db6973292fb8ad1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-01 18:49:12 +0100

    feat: :sparkles: enable @apply with unoCSS

---
+++ uno.config.js
@@ -0,0 +1,8 @@
+// uno.config.js
+import { defineConfig } from "unocss";
+import transformerDirectives from "@unocss/transformer-directives";
+
+export default defineConfig({
+  // ...
+  transformers: [transformerDirectives()],
+});
