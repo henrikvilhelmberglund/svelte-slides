30895407f4fdaf932012a48de5db02327df947aa
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-01 18:56:08 +0100

    feat: :sparkles: add better HMR support using vite:afterUpdate

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,39 @@
 <script>
-  import 'reveal.js/dist/reveal.css';
+  if (import.meta.hot) {
+    import.meta.hot.on("vite:beforeUpdate", () => {
+      // console.log("Running before update!!");
+    });
+    import.meta.hot.on("vite:afterUpdate", () => {
+      // console.log("Running after update!!");
+      // https://github.com/hakimel/reveal.js/issues/1177#issuecomment-86051667
+      deck.slide(deck.getIndices().h, deck.getIndices().v);
+    });
+  }
 
-    import Reveal            from 'reveal.js';
-    import { onMount, tick } from 'svelte';
-    import Presentation      from './Presentation.svelte';
+  import "reveal.js/dist/reveal.css";
 
-    export let app;
-    export let reveal;
+  import Reveal from "reveal.js";
+  import { onMount, tick } from "svelte";
+  import Presentation from "./Presentation.svelte";
 
-    onMount(async () => {
-        await tick();
-        const deck = new Reveal(reveal);
-        deck.initialize();
-      });
-
+  export let app;
+  export let reveal;
+  let deck;
+  onMount(async () => {
+    await tick();
+    deck = new Reveal(reveal);
+    deck.initialize();
+  });
+  
 </script>
 
 <svelte:head>
-    <title>{app.name}</title>
+  <title>{app.name}</title>
 </svelte:head>
 
 <div class="reveal">
-    <div class="slides">
-        <Presentation/>
-    </div>
+  <div class="slides">
+    <Presentation />
+  </div>
 </div>
 
